a73359b7b1739e6e226b33f3a7e6c5ba4e71fec4
Leo <wangyw.dev@outlook.com>
2025-09-02 14:38:01 +0800

    clean up

--- TicketCollection/Misc/TCFonts.swift
+++ TicketCollection/Misc/TCFonts.swift
@@ -8,92 +8,56 @@
 import SwiftUI
 
 extension Font {
-    //未使用: "STHeitiSC-Medium",
-//    static func tc宋体(_ fixedSize: CGFloat) -> Font {
-//        return .custom("SimSun", fixedSize: fixedSize)
-//    }
-//    static func tc仿宋(_ fixedSize: CGFloat) -> Font {
-//        return .custom("FangSong_GB2312", fixedSize: fixedSize)
-//    }
-//    static func tc华文宋体(_ fixedSize: CGFloat) -> Font {
-//        return .custom("STSong", fixedSize: fixedSize)
-//    }
-//    static func tc黑体(_ fixedSize: CGFloat) -> Font {
-//        return .custom("SimHei", fixedSize: fixedSize)
-//    }
-//    static func tcTechnicBold(_ fixedSize: CGFloat) -> Font {
-//        return .custom("Technic-Bold", fixedSize: fixedSize)
-//    }
-//    static func tcArialUMS(_ fixedSize: CGFloat) -> Font {
-//        return .custom("Arial Unicode MS", fixedSize: fixedSize)
-//    }
     
     static var tc左上红编号: Font {
-        //return tcArialUMS(18);
         return .custom("SourceHanSansSC-Regular", fixedSize: 18).weight(.regular)
     }
     static var tc右上检票: Font {
-        //return tc华文宋体(12);
         return .custom("SourceHanSansSC-Regular", fixedSize: 12).weight(.regular)
     }
     static var tc车次字母: Font {
-        //return tc宋体(20);
         return .custom("FreeSerif", fixedSize: 20).weight(.regular)
     }
     static var tc车次数字: Font {
-        //return tc宋体(20)
         return .custom("FreeSerif", fixedSize: 20).weight(.regular)
     }
     static var tc车站中文: Font {
-        //return tc黑体(20);
         return .custom("SourceHanSansSC-Regular", fixedSize: 20)
     }
     static var tc站: Font {
-        //return tc宋体(15);
         return .custom("SourceHanSerifSC-Bold", fixedSize: 14)
     }
     static var tc车站英文: Font {
-        //return tc宋体(12);
         return .custom("FreeSerif", fixedSize: 12).weight(.bold)
     }
     static var tc日期数字英文: Font {
-        //return tcTechnicBold(17)
         return .custom("Gilroy", fixedSize: 15)
     }
     static var tc日期中文: Font {
-        //return tc宋体(9)
         return .custom("SourceHanSerifSC-Bold", fixedSize: 10)
     }
     static var tc座位数字英文: Font {
-        //return tcTechnicBold(16);
         return .custom("Gilroy", fixedSize: 14)
     }
     static var tc座位中文: Font {
-        //return tc宋体(9)
         return .custom("SourceHanSerifSC-Bold", fixedSize: 10)
     }
     static var tc座位中文大: Font {
-        //return tc宋体(14)
         return .custom("SourceHanSerifSC-Bold", fixedSize: 14)
     }
     static var tc价格CNY: Font {
-        //return tc宋体(16)
         return .custom("PingFang SC", fixedSize: 14).weight(.light)
     }
     static var tc席别: Font {
-        //return tc黑体(13)
         return .custom("SourceHanSansSC-Regular", fixedSize: 13)
     }
     static var tc提示标签中文: Font {
-        //return tc华文宋体(12);
         return .custom("SourceHanSerifSC-Bold", fixedSize: 12)
     }
     static var tc乘客姓名: Font {
-        //return tc宋体(15);
         return .custom("SourceHanSerifSC-Bold", fixedSize: 14)
     }
     static var tc乘客身份证: Font {
-        //return tcTechnicBold(15);
         return .custom("Gilroy", fixedSize: 14).weight(.bold)
         // Note: only Gilroy Light is free. We add stroke on the text to immitate Bold
     }
@@ -101,7 +65,6 @@ extension Font {
         return .custom("SourceHanSerifSC-Bold", fixedSize: 10)
     }
     static var tc左下黑编号: Font {
-        //return tc仿宋(14);
         return .custom("FreeSerif", fixedSize: 13)
     }
     static var tc背景CR: Font {
